d96ed95d420c9a09ad594e86761eeebfcd2d40d4
JonasBerx <jonas.berx@student.ucll.be>
2019-10-09 16:22:57 +0200

    Bank methods added

--- src/domain/Bank.java
+++ src/domain/Bank.java
@@ -1,4 +1,43 @@
 package domain;
 
+import java.util.HashMap;
+
 public class Bank implements Observable {
+    private String naam;
+    private HashMap<String, Rekening> rekeningen;
+
+    public Bank(String naam) {
+        setNaam(naam);
+    }
+
+
+
+    public void setNaam(String naam) {
+        if (naam.isEmpty()) {
+            throw new IllegalArgumentException("Empty name");
+        }
+        this.naam = naam;
+
+    }
+
+    public void withdraw(String rekeningnummer, double amount) {
+        Rekening rekening = rekeningen.get(rekeningnummer);
+        rekening.withdraw(amount);
+
+    }
+
+    public void deposit(String rekeningnummer, double amount) {
+        Rekening rekening = rekeningen.get(rekeningnummer);
+        rekening.deposit(amount);
+    }
+
+    public void create(String rekeningnummer) {
+        Rekening rekening = new Rekening(rekeningnummer);
+        rekeningen.put(rekeningnummer, rekening);
+    }
+
+    public void delete(String rekeningnummer) {
+        rekeningen.remove(rekeningnummer);
+        System.out.println("Yeet");
+    }
 }
