8f5184d189a451f6641c5990468e19b4f783e758
DieterDraelants <dieter.draelants@student.ucll.be>
2019-10-09 16:18:08 +0200

    Rekeningen class done

--- src/domain/Rekening.java
+++ src/domain/Rekening.java
@@ -1,4 +1,45 @@
 package domain;
 
 public class Rekening {
+    private String rekeningnummer;
+    private double balance;
+
+    public Rekening(String rekeningnummer, double balance) {
+        this.rekeningnummer = rekeningnummer;
+        this.balance = balance;
+    }
+
+    public Rekening(String rekeningnummer) {
+        this(rekeningnummer, 0);
+    }
+
+    public void deposit(double moneys) {
+        if (moneys < 0) {
+            throw new IllegalArgumentException("Je kan geen negatief bedrag storten");
+        }
+        balance += moneys;
+    }
+
+    public void withdraw(double moneys) {
+        if (moneys > balance) {
+            throw new IllegalArgumentException("Je kan niet meer opvragen dan je hebt");
+        }
+        balance -= moneys;
+    }
+
+    public String getRekeningnummer() {
+        return rekeningnummer;
+    }
+
+    public void setRekeningnummer(String rekeningnummer) {
+        this.rekeningnummer = rekeningnummer;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
